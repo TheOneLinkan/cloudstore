2ab999a59cc8ae6409f4f8d9bcdd902d50e71cb3
Linkan <thelinussandelin@gmail.com>
2026-05-14 18:33:58 +0200

    Add OrderResponse DTO and handle null-safe product enrichment

--- user-order-service/src/main/java/se/jensen/linkan/userorderservice/dto/Product.java
+++ user-order-service/src/main/java/se/jensen/linkan/userorderservice/dto/Product.java
@@ -1,5 +1,12 @@
 package se.jensen.linkan.userorderservice.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
 
     private Long id;
@@ -8,55 +15,4 @@ public class Product {
     private String description;
     private String category;
     private String image;
-
-    public Product() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 }
