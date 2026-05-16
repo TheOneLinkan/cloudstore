585cca1b4023cfee6c50084ec22b7143fa4f95bb
Linkan <thelinussandelin@gmail.com>
2026-05-16 17:29:11 +0200

    Add Docker setup with MySQL and test configuration

--- user-order-service/src/test/java/se/jensen/linkan/userorderservice/UserOrderServiceApplicationTests.java
+++ user-order-service/src/test/java/se/jensen/linkan/userorderservice/UserOrderServiceApplicationTests.java
@@ -2,12 +2,15 @@ package se.jensen.linkan.userorderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UserOrderServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
 
 }
