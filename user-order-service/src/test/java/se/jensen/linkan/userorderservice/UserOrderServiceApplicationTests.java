70912dc7ec218ef48d5f01ea068b859d473c82b0
Linkan <thelinussandelin@gmail.com>
2026-05-27 11:11:40 +0200

    Add Bootstrap setup for frontend styling

--- user-order-service/src/test/java/se/jensen/linkan/userorderservice/UserOrderServiceApplicationTests.java
+++ user-order-service/src/test/java/se/jensen/linkan/userorderservice/UserOrderServiceApplicationTests.java
@@ -2,15 +2,13 @@ package se.jensen.linkan.userorderservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "spring.profiles.active=test"
+})
 class UserOrderServiceApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
-
 }
